3d8b135d5dc57084c6b256d3323d9da980436667
Frederic Andre <frederic.andre@kwanko.com>
2017-04-12 14:22:30 +0200

    Fix mix.exs
    Please enter the commit message for your changes. Lines starting

--- apps/exconfserver/mix.exs
+++ apps/exconfserver/mix.exs
@@ -38,8 +38,8 @@ defmodule Exconfserver.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [{:poison, "~> 3.0"},
-     {:uuid, "~> 1.1" }
+     {:uuid, "~> 1.1" },
      {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
-     {:credo, "~> 0.7", only: [:dev, :test]]
+     {:credo, "~> 0.7", only: [:dev, :test]}]
   end
 end
