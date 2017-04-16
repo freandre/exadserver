66fb352013dae07b8488f0c873145534f792f520
freandre <sysmic@free.fr>
2017-04-16 20:08:07 +0200

    Cleanup

--- apps/exadserver/lib/exadserver/indexes/infiniteKeyProcessor.ex
+++ apps/exadserver/lib/exadserver/indexes/infiniteKeyProcessor.ex
@@ -18,8 +18,7 @@ defmodule ExAdServer.Indexes.InfiniteKeyProcessor do
     ret = targeterMetada
     |> Enum.filter_map(fn ({_, v}) -> v["type"] == "infinite" end,
                        fn ({k, v}) ->
-                         createBagStore(getIxAtom(k))
-                         createBagStore(getIxAtom(k <> "Full"))
+                         createBagStore(getIxAtom(k))                      
                          {k, ExAdServer.Indexes.InfiniteKeyProcessor, v}
                        end)
     Logger.debug fn -> "[InfiniteKeyProcessor] - Exiting generateMetadata returning:\n#{inspect(ret)}" end
@@ -34,86 +33,9 @@ defmodule ExAdServer.Indexes.InfiniteKeyProcessor do
       Logger.debug fn -> "[InfiniteKeyProcessor] - generateAndStoreIndex storing:\n#{inspect(conf_data)}" end
       Enum.each(conf_data, &(ETS.insert(getIxAtom(indexName), {&1, ad_conf["adid"]})))
     end
-    Enum.each(conf_data, &(ETS.insert(getIxAtom(indexName <> "Full"), {{conf_inclusive, &1}, ad_conf["adid"]})))
   end
 
-  def findInIndex(ad, name, acc), do: findInIndexETSIncludeExclude(ad, name, acc)
-
-  def findInIndexETSIncludeExclude(adRequest, {index_name, _}, accumulator) do
-    Logger.debug fn -> "[InfiniteKeyProcessor] - findInIndex request #{index_name}:\n#{inspect(adRequest)}\n#{inspect(accumulator)}" end
-    value = adRequest[index_name]
-
-    # Get the inclusive value
-    ret = ETS.lookup(getIxAtom(index_name <> "Full"), {true, value})
-    |> Enum.map(fn({_, ad_id}) -> ad_id end)
-
-    Logger.debug fn -> "> inclusion value #{index_name} => #{inspect(ret)}" end
-
-    retAll = ETS.lookup(getIxAtom(index_name <> "Full"), {true, "all"})
-    |> Enum.map(fn({_, ad_id}) -> ad_id end)
-
-    Logger.debug fn -> "> inclusion all #{index_name} => #{inspect(retAll)}" end
-
-    others = ETS.select(getIxAtom(index_name <> "Full"), ETS.fun2ms(fn({{inclusive, storedValue}, id})
-                            when
-                              (inclusive == false and storedValue != value
-                                and storedValue != "all")
-                            ->
-                              id
-                            end))
-
-    Logger.debug fn -> "> inclusion others #{index_name} => #{inspect(ret)}" end
-
-    ret = ListUtils.intersect(accumulator, ret ++ retAll ++ others)
-
-    Logger.debug fn -> "> intersection #{index_name} => #{inspect(ret)}" end
-
-    # Exclude
-    exclude = ETS.lookup(getIxAtom(index_name <> "Full"), {false, value})
-    |> Enum.map(fn({_, ad_id}) -> ad_id end)
-
-    ret = ret -- exclude
-
-    Logger.debug fn -> "> exclusion #{index_name} => #{inspect(ret)}" end
-
-    ret
-  end
-
-  def findInIndexETSList(adRequest, {index_name, _}, accumulator) do
-    Logger.debug fn -> "[InfiniteKeyProcessor] - findInIndex #{index_name}:\n#{inspect(adRequest)}\n#{inspect(accumulator)}" end
-    value = adRequest[index_name]
-
-    # Get the inclusive value
-    ad_ids = ETS.lookup(getIxAtom(index_name), value)
-    |> Enum.map(fn({_, ad_id}) -> ad_id end)
-
-    # Add the inclusive all
-    ad_ids_all = ETS.lookup(getIxAtom(index_name), "all")
-    |> Enum.map(fn({_, ad_id}) -> ad_id end)
-
-    inter = ListUtils.intersect(accumulator, ad_ids ++ ad_ids_all)
-    Logger.debug fn -> "> inclusion #{index_name} => #{inspect(inter)}" end
-
-    # In the remaining conf id, check the excluding not value pattern
-    exclude = Enum.reduce(accumulator -- inter, [],
-                fn(ad_id, acc) ->
-                  [{^ad_id, ad_conf}] = ETS.lookup(:ads_store, ad_id)
-                    conf_inclusive = ad_conf["targeting"][index_name]["inclusive"]
-                    conf_data = ad_conf["targeting"][index_name]["data"]
-
-                    cond do
-                      (conf_inclusive == false and ((value in conf_data) == false))
-                        -> [ad_id | acc]
-                      true -> acc
-                    end
-                end)
-
-    Logger.debug fn -> "> exclusion #{index_name} => #{inspect(exclude)}" end
-
-    inter ++ exclude
-  end
-
-  def findInIndexETSLookup(adRequest, {indexName, _}, accumulator) do
+  def findInIndex(adRequest, {indexName, _}, accumulator) do
     Logger.debug fn -> "[InfiniteKeyProcessor] - findInIndex request #{indexName}:\n#{inspect(accumulator)}" end
     value = adRequest[indexName]
 
@@ -139,6 +61,5 @@ defmodule ExAdServer.Indexes.InfiniteKeyProcessor do
 
   def cleanup(indexName, _) do
     deleteStore(getIxAtom(indexName))
-    deleteStore(getIxAtom(indexName <> "Full"))
   end
 end
